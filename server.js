4987986a5d8278b57c488b6cd48ec6d626b2d5ac
Andii <andii.magyar@gmail.com>
2021-04-18 12:35:55 -0700

    Completed POST route

--- server.js
+++ server.js
@@ -1,46 +1,87 @@
 const express = require('express');
 const app = express();
-const { notes } = require('./db/db.json');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const util = require("util");
+const fs = require('fs');
+
+var readFile = util.promisify(fs.readFile);
+var writeFile = util.promisify(fs.writeFile);
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended:true}));
 
-//function createNewNote(body, notesArray) {
-  //  const notes = body;
-    //notesArray.push(notes);
-    //fs.writeFileSync(
-      //  path.join(__dirname, './db/db.json'),
-        //JSON.stringify({ notes: notesArray }, null, 2)
-    //);
-   // return notes;
-//}
-
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    readFile(
+        path.join(__dirname, '/db/db.json'),
+        "utf8"
+    )
+    .then(
+        data => {
+            res.json(JSON.parse(data))
+        }
+    )
+   
   });
 
   app.post('/api/notes', (req, res) => {
 
-    console.log(req.body);
-    res.json(req.body);
-    req.body.id = notes.length.toString();
+    readFile(
+        path.join(__dirname, '/db/db.json'),
+        "utf8"
+    )
+    .then(
+        data => {
+            const notes = JSON.parse(data)
 
-    //const notes = createNewNote(req.body, notes);
-      //  res.json(notes);
- 
+            notes.push(req.body)
+
+            writeFile (
+                path.join(__dirname, '/db/db.json'),
+                JSON.stringify(notes)
+            )
+
+            .then (
+                response => {
+                    res.json(response)
+                }
+            )
+
+        })
+
   });
 
+  app.delete('/api/notes/:id', (req, res) => {
+
+    //req.params.id
+
+    readFile(
+        path.join(__dirname, '/db/db.json'),
+        "utf8"
+    )
+    .then(
+        data => {
+            const notes = JSON.parse(data)
+
+            var filteredNotes = notes.filter 
+
+        })
+  })
+
+
   app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
     });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
+  //add id to req.body in POST 
+
+  //in delete rte where eadh notes id != that req.params(note.id) 
